6fa0a188290b333b38e1ada2b0fafb8c85fc826f
Janne Väisänen <janva415@gmail.com>
2022-05-29 20:00:55 +0300

    Add code for dcmotor project.

---
+++ dcmotor/dcmotor.ino
@@ -0,0 +1,19 @@
+const int switch_pin = 2;
+const int motor_pin = 9;
+int switch_state = 0;
+
+void setup (){
+    pinMode(motor_pin, OUTPUT);
+    pinMode(switch_pin, INPUT);
+}
+
+void loop(){
+
+    switch_state = digitalRead(switch_pin);
+    if (switch_state == HIGH){
+        digitalWrite(motor_pin, HIGH);
+    }else{
+        digitalWrite(motor_pin, LOW);
+        
+    }
+}
